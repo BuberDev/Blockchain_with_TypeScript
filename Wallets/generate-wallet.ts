5b485c4ffc081d96746e93115d8403f9fbc339a6
Dawid Bubernak <92649325+BuberDev@users.noreply.github.com>
2022-11-26 22:36:51 +0100

    Add files via update

---
+++ Wallets/generate-wallet.ts
@@ -0,0 +1,22 @@
+import { ec } from './utils';
+import { getWallets, writeWallets } from './utils/blockchain-helper';
+
+
+const newWalletName = process.argv[2];
+const currentWallets = getWallets();
+
+if (currentWallets[newWalletName])
+  throw Error('The account name already exist');
+
+
+const newPair = ec.genKeyPair();
+const newPublicKey = newPair.getPublic('hex');
+const newPrivateKey = newPair.getPrivate('hex');
+const newWallet = {
+  [newWalletName]: {
+    publicKey: newPublicKey,
+    privateKey: newPrivateKey,
+  },
+};
+
+writeWallets({ ...currentWallets, ...newWallet });
